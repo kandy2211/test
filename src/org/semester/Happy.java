a867a064837e633091d4e768075e5f294699825d
win7 <win7@win7-PC>
2023-02-06 20:29:21 +0530

    8.29

--- src/org/semester/Happy.java
+++ src/org/semester/Happy.java
@@ -1,6 +1,14 @@
 package org.semester;
 
 public class Happy {
+	public void sam() {
+System.out.println("51655555");
+System.out.println("555");
+System.out.println("516");
+System.out.println("kanday");
+
+	}
+	
 public static void main(String[] args) {
 	System.out.println("happy for everything");
 }
